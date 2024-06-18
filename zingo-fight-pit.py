e1551543c2efa00061b6ff9bf23b1b68fdae6797
Bryan Haley <bryanhaley.professional@gmail.com>
2024-06-17 17:26:11 -0700

    Render bubble first

--- zingo-fight-pit.py
+++ zingo-fight-pit.py
@@ -127,10 +127,10 @@ if __name__ == "__main__":
         if time.time() < LAST_COMMAND_TIME+RENDERING_TIMEOUT_SECONDS:
             try:
                 for chatter in chatters:
-                    screen.blit(chatters[chatter].get_name_text(), (chatters[chatter].get_rect().centerx-(chatters[chatter].get_name_text().get_rect().width/2), chatters[chatter].get_rect().top - 20))
-                    screen.blit(pygame.transform.flip(chatters[chatter].get_img(), chatters[chatter].get_flip(), False), chatters[chatter].get_rect(), chatters[chatter].get_crop_square())
                     if chatters[chatter].get_defended():
                         screen.blit(bubble_img, (chatters[chatter].get_rect().centerx-(bubble_img.get_rect().width/2), chatters[chatter].get_rect().centery-(bubble_img.get_rect().height/2)))
+                    screen.blit(chatters[chatter].get_name_text(), (chatters[chatter].get_rect().centerx-(chatters[chatter].get_name_text().get_rect().width/2), chatters[chatter].get_rect().top - 20))
+                    screen.blit(pygame.transform.flip(chatters[chatter].get_img(), chatters[chatter].get_flip(), False), chatters[chatter].get_rect(), chatters[chatter].get_crop_square())
             except:
                 print(traceback.format_exc())
         pygame.display.flip()
